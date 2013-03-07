5d749d49d30b41417e6e54d30c67220720b27708
lhendrickson3@my.wctc.edu <>
2013-03-07 14:49:24 -0600

    Created Employee class for Employee object data Edited EmployeePart2 with output and add. to an ArrayList

--- src/listandsetlab/EmployeePart2.java
+++ src/listandsetlab/EmployeePart2.java
@@ -1,6 +1,25 @@
 
 package listandsetlab;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class EmployeePart2 {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("1", "Laura", "Hendrickson");
+        Employee e2 = new Employee("2", "Tim", "Smith");
+        Employee e3 = new Employee("3", "Bob", "White");
     
+        List<Employee> employee = new ArrayList<Employee>();
+        employee.add(e1);
+        employee.add(e2);
+        employee.add(e3);
+        
+        System.out.println(employee.size());
+        
+        for(Employee e : employee){
+            System.out.println(e);
+        }
+    }
 }
